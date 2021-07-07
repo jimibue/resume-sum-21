6d2e723d744d9c5298497aba5ac041295a6abdeb
James Yeates <jimibue@gmail.com>
2021-07-07 10:14:30 -0600

    api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [things, setThings] = useState([]);
+  useEffect(() => {
+    getThings();
+  }, []);
+
+  const getThings = async () => {
+    try {
+      let res = await axios.get(
+        "https://james-sum21-demo.herokuapp.com/api/things"
+      );
+      console.log("res", res);
+      setThings(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <h1>Home Page</h1>
+      {things && things.length === 0 && <p>0 things</p>}
+      {things && things.map((t) => <p>{t.name}</p>)}
     </div>
   );
 };
